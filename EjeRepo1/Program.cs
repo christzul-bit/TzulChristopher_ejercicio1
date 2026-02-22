39045bba5ca9254e0a704e6cea76034f912b3846
chris <chris@LAPTOPCHRIS>
2026-02-22 10:04:22 -0600

    switch_caso3y4

--- EjeRepo1/Program.cs
+++ EjeRepo1/Program.cs
@@ -1,9 +1,10 @@
 ﻿//programa con repo 1
 int prioridad = 0;
+string sugerencia = "";
 Console.WriteLine("Ingrese el tipo de atención \n" +
     "1)Emergencia \n" +
     "2)Consulta \n" +
-    "3) Pediatria \n" +
+    "3)Pediatria \n" +
     "4)Traumatología_");
 int opcion = int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese la edad del paciente_");
@@ -20,8 +21,8 @@ if (edad <= 0 && temperatura > -100 && temperatura < 100 && oxigeno > 0 && dolor
     switch(opcion)
     {
         case 1:
-            //se sumara en 1 la prioridad por cada caso que se presente, al ser una emergencia la prioridad empieza con 1
-            prioridad += 1;
+            //se sumara en 3 la prioridad por cada caso que se presente, al ser una emergencia la prioridad empieza con 1
+            prioridad += 3;
             if(oxigeno < 90){prioridad += 1;}
             if(edad > 60) { prioridad += 1;}
             if(temperatura > 37) { prioridad += 1;}
@@ -35,7 +36,39 @@ if (edad <= 0 && temperatura > -100 && temperatura < 100 && oxigeno > 0 && dolor
             if (tension >= 140) { prioridad += 1; }
             if (dolor >= 5) { prioridad += 1; }
             break;
+        case 3:
+            if(edad < 18)
+            {
+                prioridad += 1;
+                if (oxigeno < 90) { prioridad += 1; }
+                if (edad < 6) { prioridad += 1; }
+                if (temperatura > 37) { prioridad += 1; }
+                if (tension >= 140) { prioridad += 1; }
+                if (dolor >= 5) { prioridad += 1; }
+            }
+            else
+            {
+                Console.WriteLine("El pasiente no es un niño");
+            }
+            break;
+        case 4:
+            prioridad += 2;
+            if (oxigeno < 90) { prioridad += 1; }
+            if (edad > 60) { prioridad += 1; }
+            if (temperatura > 37) { prioridad += 1; }
+            if (tension >= 140) { prioridad += 1; }
+            if (dolor >= 5) { prioridad += 1; }
+            break;
     }
+    if(prioridad < 4)
+    {
+        sugerencia = "Consulta normal";
+    }else if(prioridad < 6)
+    {
+        sugerencia = "Ateción necesaria";
+    }else { sugerencia = "Emergencia total"; }
+    Console.WriteLine($"El nivel de prioridad es {prioridad} \n" +
+        $"se recomienda {sugerencia}");
 }else
 {
     Console.WriteLine("Una o varios valores son invalidos, verifique bien sus respuestas");
