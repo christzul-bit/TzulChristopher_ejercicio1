c357760f9dc74e2d1ca1a60ba4211c827f525cd0
chris <chris@LAPTOPCHRIS>
2026-02-21 23:46:27 -0600

    switch_caso1y2

--- EjeRepo1/Program.cs
+++ EjeRepo1/Program.cs
@@ -1,4 +1,5 @@
 ﻿//programa con repo 1
+int prioridad = 0;
 Console.WriteLine("Ingrese el tipo de atención \n" +
     "1)Emergencia \n" +
     "2)Consulta \n" +
@@ -15,4 +16,27 @@ Console.WriteLine("Ingrese el nivel de dolor en el rango 1-10_");
 int dolor = int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese la tensión sistólica_");
 double tension = double.Parse(Console.ReadLine());
-
+if (edad <= 0 && temperatura > -100 && temperatura < 100 && oxigeno > 0 && dolor >= 1 && dolor <= 100 && tension > 0) { 
+    switch(opcion)
+    {
+        case 1:
+            //se sumara en 1 la prioridad por cada caso que se presente, al ser una emergencia la prioridad empieza con 1
+            prioridad += 1;
+            if(oxigeno < 90){prioridad += 1;}
+            if(edad > 60) { prioridad += 1;}
+            if(temperatura > 37) { prioridad += 1;}
+            if(tension >= 140) { prioridad += 1;}
+            if(dolor >= 5) {  prioridad += 1;}
+            break;
+        case 2:
+            if (oxigeno < 90) { prioridad += 1; }
+            if (edad > 60) { prioridad += 1; }
+            if (temperatura > 37) { prioridad += 1; }
+            if (tension >= 140) { prioridad += 1; }
+            if (dolor >= 5) { prioridad += 1; }
+            break;
+    }
+}else
+{
+    Console.WriteLine("Una o varios valores son invalidos, verifique bien sus respuestas");
+}
