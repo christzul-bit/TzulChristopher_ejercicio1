f2034c720bc47ce22161ff824ae8c9f2bc640072
chris <chris@LAPTOPCHRIS>
2026-02-21 23:14:09 -0600

    menu y entradas

--- EjeRepo1/Program.cs
+++ EjeRepo1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//programa con repo 1
+Console.WriteLine("Ingrese el tipo de atención \n" +
+    "1)Emergencia \n" +
+    "2)Consulta \n" +
+    "3) Pediatria \n" +
+    "4)Traumatología_");
+int opcion = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la edad del paciente_");
+int edad = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la temperatura en °C_");
+double temperatura = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la saturacion de oxigeno_");
+double oxigeno = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el nivel de dolor en el rango 1-10_");
+int dolor = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la tensión sistólica_");
+double tension = double.Parse(Console.ReadLine());
+
